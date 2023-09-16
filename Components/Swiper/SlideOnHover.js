de073e70a8a9fd0e25408e9c4ca7a982b406263b
Shashi Ross <shashiross9@gmail.com>
2023-09-16 19:49:11 +0530

    fetch product api and start to show data

--- Components/Swiper/SlideOnHover.js
+++ Components/Swiper/SlideOnHover.js
@@ -1,36 +1,34 @@
-import React from 'react'
+import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
 const SlideOnHover = ({ data }) => {
-    const [play,setPlay] = React.useState(false)
-    return (
-        <div className='w-full mx-auto'>
-            <Carousel 
-                className="h-full mx-auto my-auto" 
-                infiniteLoop 
-                useKeyboardArrows 
-                autoPlay={play}
-                showArrows={false}
-                showThumbs={false}
-                showIndicators={false}
-                showStatus={false}
-            >
-                {
-                    data.map((item, index) => (
-                        <div 
-                            className='cursor-pointer' 
-                            key={index} 
-                            onMouseEnter={()=>setPlay(true)} 
-                            onMouseLeave={()=>setPlay(false)}
-                        >
-                            <img src={item.src} className="" />
-                        </div>
-                    ))
-                }
-            </Carousel>
-        </div>
-    )
-}
+  const [play, setPlay] = React.useState(false);
+  return (
+    <div className="w-full mx-auto">
+      <Carousel
+        className="h-full mx-auto my-auto"
+        infiniteLoop
+        useKeyboardArrows
+        autoPlay={play}
+        showArrows={false}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+      >
+        {data.map((item, index) => (
+          <div
+            className="cursor-pointer"
+            key={index}
+            onMouseEnter={() => setPlay(true)}
+            onMouseLeave={() => setPlay(false)}
+          >
+            <img src={item.src} className="" />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
-export default SlideOnHover
+export default SlideOnHover;
