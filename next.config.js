fd2fa5efa1082f62e112ac69fca2b8449f884385
Shashi Ross <43995788+Sross123@users.noreply.github.com>
2024-01-16 02:17:02 +0530

    set base url in next.config file for api and image url and install some required module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    BASE_URL: process.env.BASE_URL || "http://3.24.191.174:5000/api/",
+    BASE_IMAGE_URL: process.env.NASE_IMAGE_URL || 'https://akkukachasma.s3.amazonaws.com/'
+  },
+  images: {
+    domains: ['https://akkukachasma.s3.amazonaws.com/product_images/'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
