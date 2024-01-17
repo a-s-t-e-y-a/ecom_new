ca78e8f12b6a2456f209b62f0893a4bc85911bdd
Shashi Ross <43995788+Sross123@users.noreply.github.com>
2024-01-17 21:11:38 +0530

    add pagination component

--- pages/sunglasses/index.js
+++ pages/sunglasses/index.js
@@ -5,6 +5,7 @@ import Layout from "@/Layout/Layout";
 import axios from "axios";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+import Pagination from "@/Components/Pagination/Pagination";
 
 const index = () => {
   const [data, setData] = useState([]);
@@ -26,20 +27,25 @@ const index = () => {
   }, []);
   return (
     <Layout>
-      <section className="flex h-auto space-x-3">
-        <div className="">
-          <Filter />
+      <section className="flex flex-col items-center h-auto space-x-3">
+        <div className="flex">
+          <div className="">
+            <Filter />
+          </div>
+          <div className="grid grid-cols-3 h-fit gap-3 gap-y-5">
+            {data &&
+              data.map((val, index) => (
+                <div
+                  key={index}
+                  onClick={() => navigateToSingleProduct(val?.products_id)}
+                >
+                  <SingleGlassItem value={val} />
+                </div>
+              ))}
+          </div>
         </div>
-        <div className="grid grid-cols-3 h-fit gap-3 gap-y-5">
-          {data &&
-            data.map((val, index) => (
-              <div
-                key={index}
-                onClick={() => navigateToSingleProduct(val?.products_id)}
-              >
-                <SingleGlassItem value={val} />
-              </div>
-            ))}
+        <div>
+          <Pagination />
         </div>
       </section>
     </Layout>
