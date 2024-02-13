6824deb9dd548946a934bdec88d089e904b634db
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-02-14 00:40:06 +0530

    modifications

--- pages/404.js
+++ pages/404.js
@@ -1,46 +1,39 @@
-import { useRouter } from "next/router";
-import React from "react";
+import Link from 'next/link'
 
 const Page404 = () => {
-  const router = useRouter
   return (
-    <>
-      {/* <div className="min-w-screen min-h-screen bg-blue-100 flex items-center p-5 lg:p-20 overflow-hidden relative">
-        <div className="flex  min-h-full min-w-full rounded-3xl bg-white shadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left">
-          <div className="mb-10 md:mb-20 text-gray-600 font-light">
-            <h1 className="font-black uppercase text-3xl lg:text-5xl text-sky-500 mb-10">
-              You seem to be lost!
-            </h1>
-            <p>The page you're looking for isn't available.</p>
-            <p>Try searching again or use the Go Back button below.</p> */}
-            <button
-              onClick={()=>router.push("/404")}
-              className="mt-7 text-base font-bold outline-none focus:outline-none transform transition-all hover:scale-110 text-indigo-500 hover:text-indigo-600">
-              Go Back
-            </button>
-          {/* </div>
-        </div>
-        <div className="w-64 md:w-96 h-96 md:h-full bg-blue-200 bg-opacity-30 absolute -top-64 md:-top-96 right-20 md:right-32 rounded-full pointer-events-none -rotate-45 transform"></div>
-        <div className="w-96 h-full bg-yellow-200 bg-opacity-20 absolute -bottom-96 right-64 rounded-full pointer-events-none -rotate-45 transform"></div>
+    <div className="bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div className="mx-auto max-w-max">
+        <main className="sm:flex">
+          <p className="bg-gradient-to-br from-pink-400 to-cyan-600 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl">
+            404
+          </p>
+          <div className="sm:ml-6">
+            <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                Page not found
+              </h1>
+              <p className="mt-1 text-base text-gray-500">
+                Please check the URL in the address bar and try again.
+              </p>
+            </div>
+            <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+              <Link href="/">
+                <button className="inline-flex items-center rounded-md  border-transparent  bg-gradient-to-br from-pink-400 to-cyan-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gradient-to-br hover:from-pink-600 hover:to-cyan-800 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
+                  Go back home
+                </button>
+              </Link>
+              <Link href="/support">
+                <button className="inline-flex items-center rounded-md border border-transparent bg-pink-100 px-4 py-2 text-sm font-medium text-black hover:bg-pink-200 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
+                  Contact support
+                </button>
+              </Link>
+            </div>
+          </div>
+        </main>
       </div>
+    </div>
+  )
+}
 
-      <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
-        <div>
-          <a
-            title=""
-            href="https://www.buymeacoffee.com/scottwindon"
-            target="_blank"
-            className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
-          >
-            <img
-              className="object-cover object-center w-full h-full rounded-full"
-              src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg"
-            />
-          </a>
-        </div>
-      </div> */}
-    </>
-  );
-};
-
-export default Page404;
+export default Page404
