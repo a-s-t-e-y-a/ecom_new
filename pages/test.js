47bb6f26ebef0434bd3e86486be418239ef1ae2b
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-02-25 18:33:25 +0530

    error

--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,11 @@
+import Errorpage from "@/Components/Error";
+
 const { default: Loader } = require("@/Components/Loader");
 
 function Test(){
     return(
         <>
-        <Loader></Loader>
+        <Errorpage></Errorpage>
         </>
     )
 }
