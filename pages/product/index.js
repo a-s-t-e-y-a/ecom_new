c825f7be28e6f402074a092c04e2b21b5e056382
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-02-25 18:35:19 +0530

    ne

--- pages/product/index.js
+++ pages/product/index.js
@@ -3,32 +3,24 @@ import SingleGlassItem from '@/Components/SingleGlassItem'
 import Layout from '@/Layout/Layout'
 import React from 'react'
 import { useRouter } from "next/router";
-import axios from "axios";
-import { env } from '@/next.config';
-import useGetAllProducts from '@/utils/queries/common/UseProductGet';
+import useGetAllProducts from '@/utils/queries/common/UseProductGetAll';
+import Loader from '@/Components/Loader';
 
 
 const Index = () => {
-  // const [data, setData] = React.useState([]); /
-  // const {BASE_URL} = env
+
+
   const router = useRouter();
-  // first comment to test
+
   const navigateToSingleProduct = (id) => {
     router.push(`/product/${id}`);
   };
-  // const fetchData = () => {
-  //   axios
-  //     .get(`${BASE_URL}products?page=&itemsPerPage=20`)
-  //     .then((result) => {
-  //       setData(result?.data?.data);
-  //     });
-  // };
-  // React.useEffect(() => {
-  //   fetchData();
-  // }, []);
   const {isLoading , isError , data }=  useGetAllProducts()
+  if(isLoading){
+    return <><Loader/></>
+ }
   if(isError){
-    return <>Loading ../</>
+    return <> </>
   }
   return (
     <Layout>
