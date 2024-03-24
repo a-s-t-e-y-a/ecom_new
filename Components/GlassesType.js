6bc2f710ba490ec4f37ab04f0f09f35864f51ddb
Shashi Ross <shashiross9@gmail.com>
2024-03-24 19:11:13 +0530

    glassess category

--- Components/GlassesType.js
+++ Components/GlassesType.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
@@ -9,37 +10,36 @@ const GlassesType = () => {
   const { data: categoryItems } = useGetAllCategories();
   const handleNavigate = (item) => {
     router.push(`${item?.name?.toLowerCase()}`);
-    console.log(categoryItems, "categoryItems");
-    return (
-      <motion.div className="w-full flex items-center gap-5 mt-6 justify-between cursor-pointer">
-        {categoryItems &&
-          categoryItems.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="flex flex-col justify-normal items-center"
-                onClick={() => handleNavigate(item)}
-              >
-                <motion.div
-                  className="rounded-full shadow-md hover:shadow-2xl"
-                  whileHover={{ scale: 0.8 }}
-                >
-                  <Image
-                    src={`/product_images/${item?.image}`}
-                    width={130}
-                    height={130}
-                    alt="Glasses Categories"
-                  />
-                </motion.div>
-                <p className="text-sm font-semibold tracking-wide mt-2">
-                  {item?.name}
-                </p>
-              </div>
-            );
-          })}
-      </motion.div>
-    );
-  };
+  }
+  console.log(categoryItems, "categoryItems");
+  return (
+    <motion.div className="w-full flex items-center gap-5 mt-6 justify-between cursor-pointer">
+      {categoryItems?.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="flex flex-col justify-normal items-center"
+            onClick={() => handleNavigate(item)}
+          >
+            <motion.div
+              className="rounded-full shadow-md hover:shadow-2xl"
+              whileHover={{ scale: 0.8 }}
+            >
+              <Image
+                src={`/product_images/${item?.image}`}
+                width={130}
+                height={130}
+                alt="Glasses Categories"
+              />
+            </motion.div>
+            <p className="text-sm font-semibold tracking-wide mt-2">
+              {item?.name}
+            </p>
+          </div>
+        );
+      })}
+    </motion.div>
+  );
 };
 
 export default GlassesType;
