04274cbe3ef1130ce91a46d9fd40185b8f4b8bca
Shashi Ross <shashiross9@gmail.com>
2024-03-26 12:21:04 +0530

    fixing all

--- utils/isAuthencate.js
+++
@@ -1,9 +0,0 @@
-import Router from "next/router";
-const Auth = () => {
-  const auth = JSON.parse(localStorage.getItem("user_info"));
-  if (!auth) {
-    Router.push("/login");
-  }
-};
-
-export default Auth;
