3decd32c8bd3acbbabe5ded323782843d20777c4
Shashi Ross <shashiross9@gmail.com>
2024-04-17 03:03:03 +0530

    sizeGuide form implemented

--- pages/admin/sizeGuide.js
+++ pages/admin/sizeGuide.js
@@ -10,7 +10,7 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 const SizeGuide = () => {
   const router = useRouter();
-
+  const [value, setValue] = useState("")
   const [logged, setlogged] = useState(false);
   useEffect(() => {
     if (IsAuth("admin_info")) {
@@ -27,8 +27,8 @@ const SizeGuide = () => {
             <IconButton label="Size Guides" icon={<TbTextSize />} />
           </div>
           <div className="mt-6 flex items-center gap-3 flex-wrap w-full">
-            <QuillEditor />
-            <button className=" bg-blue-500 rounded shadow  w-40 text-center text-white my-4">
+            <QuillEditor value={value} onChange={(val)=>setValue(val)}/>
+            <button className=" bg-blue-500 rounded shadow  w-40 text-center text-white my-4" onClick={()=>console.log(value)}>
               ADD <AddIcon />
             </button>
           </div>
