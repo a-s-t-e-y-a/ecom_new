d1a1ea25f9dfb18514653b5c0e0e890f998aa757
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-04-16 15:32:13 +0530

    new

--- Components/Admin/PowerType.js
+++ Components/Admin/PowerType.js
@@ -3,16 +3,21 @@ import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import Image from "next/image";
 import DeletePoPUPDialog from "@/Components/Dialog/DeletePoPUPDialog";
 import Modal from "../Dialog/Modal";
+
 const PowerType = (props) => {
   const { src, title, description, id, mutate } = props;
   const [Delete, setDelete] = useState(false);
   const [DeletePayload, setDeletePayload] = useState({});
+
   const onHideDelete = () => setDelete(false);
   const onShowDelete = () => setDelete(true);
-  const deletehandler = (item) => {
-    setDeletePayload(item);
+
+  const deletehandler = (itemId) => {
+    console.log(itemId);
+    mutate(itemId);
     onShowDelete();
   };
+
   return (
     <div className=" grid grid-cols-4 items-center gap-2 border-2">
       <Modal isOpen={Delete} closeModal={onHideDelete} fullWidth={false}>
@@ -23,14 +28,18 @@ const PowerType = (props) => {
         />
       </Modal>
       <div className="">
-        <img alt="" src={src} className="w-[200px] h-[100px] object-contain rounded-full" />
+        <img
+          alt=""
+          src={src}
+          className="w-[200px] h-[100px] object-contain rounded-full"
+        />
       </div>
       <div className=" col-span-2">
         <p className="font-semibold">{title}</p>
         <p className="text-sm text-gray-500">{description}</p>
       </div>
       <button
-        className="  text-red-500 cursor-pointer"
+        className=" text-red-500 cursor-pointer"
         onClick={() => deletehandler(id)}
       >
         <DeleteOutlineIcon className="text-base" />
