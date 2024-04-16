e24e3e79c2a2208ab3b95773a26c0279e39769c4
Shashi Ross <shashiross9@gmail.com>
2024-04-17 03:04:38 +0530

    cancellation

--- pages/admin/cancellation.js
+++ pages/admin/cancellation.js
@@ -10,7 +10,7 @@ import AddIcon from "@mui/icons-material/Add";
 
 const Cancellation = () => {
   const router = useRouter();
-
+  const [value, setValue] = useState("")
   const [logged, setlogged] = useState(false);
   useEffect(() => {
     if (IsAuth("admin_info")) {
@@ -27,8 +27,11 @@ const Cancellation = () => {
             <IconButton label="Cancellation" icon={<MdCancelScheduleSend />} />
           </div>
           <div className="mt-6 flex items-center gap-3 flex-wrap w-full">
-            <QuillEditor />
-            <button className=" bg-blue-500 rounded shadow  w-40 text-center text-white my-4">
+            <QuillEditor value={value} onChange={(val) => setValue(val)} />
+            <button
+              className=" bg-blue-500 rounded shadow  w-40 text-center text-white my-4"
+              onClick={() => console.log(value)}
+            >
               ADD <AddIcon />
             </button>
           </div>
