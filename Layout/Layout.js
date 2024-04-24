de5aee54446894f501c84e7b664878d9fe39734b
Shashi Ross <shashiross9@gmail.com>
2024-04-25 00:55:54 +0530

    added google analatycs

--- Layout/Layout.js
+++ Layout/Layout.js
@@ -9,16 +9,10 @@ const Layout = ({ children }) => {
   const router = useRouter();
   return (
     <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-VP5TNHPR85"
+      <meta
+        name="google-site-verification"
+        content="f_5BjOCM0Qb1adYVI5nSK3RRB1NxYwNaEF7e3My7A8Y"
       />
-      <Script id="google-analytics">{` 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-VP5TNHPR85');
-`}</Script>
       <Head>
         <title>akkukachasma</title>
       </Head>
@@ -27,13 +21,6 @@ const Layout = ({ children }) => {
       <main className=" mx-auto min-h-screen">{children}</main>
       <WhatsappIcon />
       <Footer />
-
-      <Script id="anlaytics">
-        {`
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});
-var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MJF8R8W');`}
-      </Script>
     </>
   );
 };
