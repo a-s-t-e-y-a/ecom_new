3fce3079799e6872bb8c55766c0b25817a2eaf1e
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-04-27 13:21:52 +0530

    new

--- pages/admin/deliveryShippingTerms.js
+++ pages/admin/deliveryShippingTerms.js
@@ -7,11 +7,16 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
+import { useForm, Controller } from "react-hook-form"; // Import Controller
+import { useMutation } from "@tanstack/react-query";
+import CreateDelivery from "@/utils/mutations/useCreateDeliveryTerm";
+import { toast } from "react-toastify";
 
 const DeliveryShippingTerms = () => {
   const router = useRouter();
-  const [value, setValue] = useState("")
   const [logged, setlogged] = useState(false);
+  const { control, handleSubmit } = useForm(); // Use control instead of register
+
   useEffect(() => {
     if (IsAuth("admin_info")) {
       setlogged(true);
@@ -19,6 +24,20 @@ const DeliveryShippingTerms = () => {
       router.replace("login");
     }
   }, [router]);
+  const { mutate } = useMutation({
+    mutationFn: CreateDelivery,
+    onSuccess: () => {
+      toast.success("delivery created successfully");
+    },
+    onError: (err) => {
+      toast.error("Error occurred");
+    },
+  });
+  const onSubmit = (data) => {
+    // Handle form submission
+    mutate(data)
+  };
+
   if (logged) {
     return (
       <AdminLayout>
@@ -30,10 +49,25 @@ const DeliveryShippingTerms = () => {
             />
           </div>
           <div className="mt-6 flex items-center gap-3 flex-wrap w-full">
-            <QuillEditor value={value} onChange={(val) => setValue(val)} />
-            <button className=" bg-blue-500 rounded shadow  w-40 text-center text-white my-4" onClick={() => console.log(value)}>
-              ADD <AddIcon />
-            </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Controller
+                name="description" // The name of the field in the form state
+                control={control} // The control object from useForm
+                render={({ field }) => (
+                  <QuillEditor
+                    {...field} // Spread the field props onto the QuillEditor
+                    onChange={field.onChange} // Bind the onChange event to field.onChange
+                  />
+                )}
+              />
+
+              <button
+                type="submit"
+                className=" bg-blue-500 rounded shadow w-40 text-center text-white my-4"
+              >
+                ADD <AddIcon />
+              </button>
+            </form>
           </div>
         </div>
       </AdminLayout>
