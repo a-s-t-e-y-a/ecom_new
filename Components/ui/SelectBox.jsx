aabce05b1d2417eb74be8af4aba503e5cc7b58f4
Shashi Ross <shashiross9@gmail.com>
2024-05-06 12:30:56 +0530

    create custom selectBox component

--- Components/ui/SelectBox.jsx
+++ Components/ui/SelectBox.jsx
@@ -1,9 +1,4 @@
-import {
-  Autocomplete,
-  FormControl,
-  InputLabel,
-  TextField,
-} from "@mui/material";
+import { Autocomplete, FormControl, TextField } from "@mui/material";
 import React from "react";
 
 const SelectBox = ({
@@ -11,24 +6,37 @@ const SelectBox = ({
   label = "",
   value,
   onChange,
+  multiple,
   options = [],
 }) => {
-
-  const selectedValue = React.useMemo(
-    () => value,
-    [value],
-  );
+  const selectedValue = React.useMemo(() => {
+    if (typeof value === "object") {
+      // Handle object type values
+      return options.find((option) => option.id === value.id) || null;
+    } else {
+      // Handle primitive type values
+      return options.find((option) => option === value) || null;
+    }
+  }, [value, options]);
+  console.log(options, "options")
+  console.log(value, 'value')
+  console.log(selectedValue, 'selectedValue')
 
   return (
     <FormControl sx={{ width: 300 }} size={size}>
       <Autocomplete
         disablePortal
-        id="gender"
         options={options}
+        getOptionKey={(option) => option.id || option}
+        getOptionLabel={(option) => option.label || option}
+        autoHighlight 
+        multiple={multiple ? true : false}
         sx={{ width: 300 }}
         value={selectedValue}
         onChange={(_, newValue) => onChange(newValue)}
-        renderInput={(params) => <TextField {...params} label={label} />}
+        renderInput={(params) => (
+          <TextField {...params} label={label}/>
+        )}
       />
     </FormControl>
   );
