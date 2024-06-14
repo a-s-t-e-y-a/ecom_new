a64a3ad925a2f6a1427d34c54bb1c722f6c66a9f
Shashi Ross <shashiross9@gmail.com>
2024-06-14 15:45:13 +0530

    impliment some new feature on eyeglasses and sunglasses

--- Components/MenuComponentNested.js
+++ Components/MenuComponentNested.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import Link from "next/link";
-// import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -11,10 +11,10 @@ const MenuComponentNested = (props) => {
   const { title, items } = props;
   return (
     <Menu as="div" className=" inline-block text-left">
-      <div>
+      <div className="flex items-center justify-center">
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2  text-sm  md:text-lg  font-semibold text-gray-900 shadow-sm  ring-gray-300 hover:bg-gray-50">
           {title}
-          {/* <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" /> */}
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         </Menu.Button>
       </div>
 
@@ -28,14 +28,14 @@ const MenuComponentNested = (props) => {
         leaveTo="transform scale-95 opacity-0"
       >
         <Menu.Items className=" absolute z-10  max-md:left-1/2 max-md:-translate-x-1/2 max-md:w-full  w-fit flex gap-auto origin-top-left rounded-md bg-white shadow-lg ring-1 focus:outline-none">
-          {items.map((link, i) => (
+          {items?.map((link, i) => (
             /* Use the `active` state to conditionally style the active item. */
             <div key={i} className="py-2 text-center flex">
               <Menu.Item key={link.href} as={Fragment} className="">
                 {({ active }) => (
                     <div className="">
                       <p className="font-bold tracking-wide py-2">{link.label}</p>
-                      {link.subItems.map((link, i) => (
+                      {link?.subItems?.map((link, i) => (
                         <div key={i} className="py-1 text-center ">
                           <Menu.Item key={link.href} as={Fragment}>
                             {({ active }) => (
