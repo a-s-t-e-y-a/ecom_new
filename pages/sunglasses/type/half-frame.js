76abc861269ce52a5ff485adc5b5c34efed3452f
Shashi Ross <shashiross9@gmail.com>
2024-06-18 12:46:36 +0530

    removed unneccesary code

--- pages/sunglasses/type/half-frame.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const halfFrame = () => {
-  return (
-    <div>half-frame</div>
-  )
-}
-
-export default halfFrame
--- pages/sunglasses/type/rimless.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const rimless = () => {
-  return (
-    <div>rimless</div>
-  )
-}
-
-export default rimless
