e043681606d464ff0feea59ae0a8b3619bb1a858
Shashi Ross <shashiross9@gmail.com>
2024-06-24 14:58:32 +0530

    try@home

--- pages/admin/try@Home.js
+++ pages/admin/try@Home.js
@@ -14,36 +14,21 @@ import useGetAllTryAtHome from "@/utils/queries/useTryAtHomeGetAll";
 import DeleteTryAtHome from "@/utils/mutations/useDeleteTryAtHome";
 import { useMutation } from "@tanstack/react-query";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import DeletePoPUPDialog from "@/Components/Dialog/DeletePoPUPDialog";
 const TryAtHome = () => {
-  // const router = useRouter();
-  // const [open, setOpen] = useState(false);
-  // const handleOpen = () => setOpen(!open);
-  // const onHide = () => setOpen(false);
-  // const [logged, setlogged] = useState(false);
-  // const [value, setValue] = useState("");
-  // const { data, refetch } = useGetAllTryAtHome();
-  // const items = [
-  //   {
-  //     services:"home Services",
-  //   },
-  // ];
-  // useEffect(() => {
-  //   if (IsAuth("admin_info")) {
-  //     setlogged(true);
-  //   } else {
-  //     router.replace("login");
-  //   }
-  // }, [router,refetch]);
-  // if (logged) {
-  const { data, refetch } = useGetAllTryAtHome();
+  const router = useRouter();
+  const [Delete, setDelete] = useState(false);
+  const [logged, setlogged] = useState(false);
   const [open, setOpen] = useState(false);
+  const [DeletePayload, setDeletePayload] = useState({});
+  const { data, refetch } = useGetAllTryAtHome();
   const handleOpen = () => setOpen(!open);
   const onHide = () => setOpen(false);
   const [get, setget] = useState(false);
 
-  useEffect(() => {
-    refetch();
-  }, [get, refetch]);
+  const onHideDelete = () => setDelete(false);
+  const onShowDelete = () => setDelete(true);
+
   const { mutate } = useMutation({
     mutationFn: DeleteTryAtHome,
     onSuccess: (data) => {
@@ -55,14 +40,38 @@ const TryAtHome = () => {
       toast.error(err.message);
     },
   });
-  return (
-    <AdminLayout>
-      <Modal isOpen={open} closeModal={onHide} fullWidth={false}>
-        <TryHome onCancel={onHide} refecth={setget} token={get} />
-      </Modal>
 
-      <div>
-        {/* {data &&
+  const Deletehandeler = (val) => {
+    onShowDelete();
+    setDeletePayload(val);
+  };
+
+  useEffect(() => {
+    if (IsAuth("admin_info")) {
+      setlogged(true);
+    } else {
+      router.replace("login");
+    }
+    refetch();
+  }, [router, get]);
+
+  if (logged) {
+    return (
+      <AdminLayout>
+        <Modal isOpen={open} closeModal={onHide} fullWidth={false}>
+          <TryHome onCancel={onHide} refecth={setget} token={get} />
+        </Modal>
+
+        <Modal isOpen={Delete} closeModal={onHideDelete} fullWidth={false}>
+          <DeletePoPUPDialog
+            Closefunction={onHideDelete}
+            Deletefunction={mutate}
+            payload={DeletePayload}
+          />
+        </Modal>
+
+        <div>
+          {/* {data &&
             data.map((item, index) => (
               <div key={index}>
                 <TryAtHomeItem
@@ -70,10 +79,10 @@ const TryAtHome = () => {
               />
               </div>
             ))} */}
-        <div onClick={handleOpen} className="ml-4 mb-4">
-          <IconButton label="Try@Home" icon={<HiOutlineHome />} />
-        </div>
-        <div className="mt-10 mx-4 flex items-center gap-5 flex-wrap">
+          <div onClick={handleOpen} className="ml-4 mb-4">
+            <IconButton label="Try@Home" icon={<HiOutlineHome />} />
+          </div>
+          <div className="mt-10 mx-4 flex items-center gap-5 flex-wrap">
             {data &&
               data.map((color, index) => (
                 <div
@@ -81,7 +90,7 @@ const TryAtHome = () => {
                   className="border rounded-md shadow-md px-5 py-2 inline-flex items-center gap-2 bg-gray-100"
                 >
                   <span className="text-base tracking-wide font-semibold text-gray-700">
-                    {color?.name}
+                    {color?.description}
                   </span>
                   <button
                     onClick={() => Deletehandeler(color)}
@@ -92,9 +101,10 @@ const TryAtHome = () => {
                 </div>
               ))}
           </div>
-      </div>
-    </AdminLayout>
-  );
+        </div>
+      </AdminLayout>
+    );
+  }
 };
 
 export default TryAtHome;
