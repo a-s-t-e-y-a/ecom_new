bb8c3c43a6e106e3a4e723fefbafca2b005a12c2
Shashi Ross <shashiross9@gmail.com>
2024-06-24 15:59:39 +0530

    modified

--- pages/admin/franchise.js
+++ pages/admin/franchise.js
@@ -9,17 +9,13 @@ import { useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import { useForm, Controller } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
-import CreateDelivery from "@/utils/mutations/useCreateDeliveryTerm";
 import { toast } from "react-toastify";
 import { MdOutlineCurrencyFranc, MdPolicy } from "react-icons/md";
-import useCreateFranchise from "@/utils/mutations/useCreateFranchise";
-import { Textarea } from "@material-tailwind/react";
 
 const Franchise = () => {
   const router = useRouter();
   const [logged, setlogged] = useState(false);
   const { control, handleSubmit, reset } = useForm();
-  const { mutate } = useCreateFranchise();
 
   useEffect(() => {
     if (IsAuth("admin_info")) {
@@ -30,7 +26,7 @@ const Franchise = () => {
   }, [router]);
 
   const onSubmit = (data) => {
-    mutate(data);
+  
   };
 
   if (logged) {
@@ -42,30 +38,6 @@ const Franchise = () => {
               label="Franchise Page"
               icon={<MdOutlineCurrencyFranc />}
             />
-          </div>
-          <div className="mt-6 mx-4 flex items-center gap-3 flex-wrap w-full">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {/* <Controller
-                name="description"
-                control={control}
-                render={({ field }) => (
-                  <QuillEditor {...field} onChange={field.onChange} />
-                )}
-              /> */}
-              
-            <Textarea
-              label="Franchise"
-              className="w-[600px]"
-              
-            />
-          
-              <button
-                type="submit"
-                className=" bg-blue-500 rounded shadow w-40 text-center text-white my-4"
-              >
-                ADD <AddIcon />
-              </button>
-            </form>
           </div>
         </div>
       </AdminLayout>
