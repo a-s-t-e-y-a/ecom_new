4075c99f745b5c13355f995771525d27f32c378c
Shashi Ross <shashiross9@gmail.com>
2024-07-30 17:54:25 +0530

    shape slider integrated

--- Components/Header/Header1.js
+++ Components/Header/Header1.js
@@ -4,37 +4,21 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { motion } from "framer-motion";
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {Navigation} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
 import { useRouter } from "next/router";
 import { useState } from "react";
 import CartDialog from "../Dialog/CartDialog";
 import Image from "next/image";
 import axios from "axios";
 import WishlistDialog from "../Dialog/WishlistDialog";
+import useGetAllShape from "@/utils/queries/useShapeGetAll";
 
 const Header1 = () => {
   const [open, setOpen] = useState(false);
   const [openFav, setOpenFav] = useState(false);
-  const [Round, setRound] = useState([]);
+  const { data } = useGetAllShape()
   const router = useRouter();
-  const fecthMultipleData = () => {
-    const url = [
-      "https://api.akkukachasma.com/api/products",
-      "https://api.akkukachasma.com/api/categories/Eyeglass",
-      "https://api.akkukachasma.com/api/shape",
-    ];
-    axios.all(url.map((endpoints) => axios.get(endpoints))).then((data) => {
-      const AllProduct = data[0];
-      const round = AllProduct?.data?.data?.products?.filter(
-        (Value) => Value?.shape_?.name === "Round" || "round"
-      );
-      setRound(round);
-    });
-  };
-  useEffect(() => {
-    fecthMultipleData();
-  }, []);
   return (
     <>
       <CartDialog open={open} setOpen={setOpen} />
@@ -52,44 +36,46 @@ const Header1 = () => {
           />
         </div>
 
-        <div className="flex-grow h-full mt-4  overflow-hidden">
+        <div className="flex-grow w-full h-full mt-4  overflow-hidden">
           <Swiper
             modules={[Navigation]}
             slidesPerView={4}
+            loop={true}
             autoplay
             navigation
-            className="w-full h-full"
           >
             {
-              Array.isArray(Round) && Round.map((glass, index) => {
+              data?.map((glass, index) => {
                 return (
-                  <SwiperSlide 
+                  <SwiperSlide
                     key={glass.id}
-                    className="ml-4 px-4"
+                    className="flex items-center justify-center"
                   >
-                    <Image 
-                      src= {glass.imageArray[0]}
-                      width={400}
-                      height={400}
-                      alt=""
-                    />
-                    <span className="text-sm text-center font-semibold">
-                      {glass.name}
-                    </span>
+                    <div className="flex flex-col justify-center items-center">
+                      <Image
+                        src={glass?.image}
+                        width={160}
+                        height={160}
+                        alt=""
+                      />
+                      <span className="text-xs text-center font-semibold">
+                        {glass?.name}
+                      </span>
+                    </div>
                   </SwiperSlide>
-                  
+
                 )
               })
             }
-           
+
           </Swiper>
         </div>
 
         <div className="flex flex-shrink-0  gap-3 mb-4  text-2xl text-gray-700">
           <motion.span whileTap={{ scale: 0.9 }} className="cursor-pointer">
-            <BsFillPersonFill onClick={() => router.push(`/user`)}/>
+            <BsFillPersonFill onClick={() => router.push(`/user`)} />
           </motion.span>
-          <motion.span whileTap={{ scale: 0.9 }} className="cursor-pointer" onClick={()=>setOpenFav(!openFav)}>
+          <motion.span whileTap={{ scale: 0.9 }} className="cursor-pointer" onClick={() => setOpenFav(!openFav)}>
             <AiOutlineHeart />
           </motion.span>
           {/* <motion.span whileTap={{ scale: 0.9 }}>
